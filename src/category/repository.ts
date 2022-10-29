c94f6dbcfa24f36e9cab5fd3acda6503b109ea75
Eremeow138 <eremeow138@yandex.ru>
2022-10-29 23:00:05 +0800

    init

---
+++ src/category/repository.ts
@@ -0,0 +1,551 @@
+import { Category } from './category';
+
+const categories: Category[] = [
+  {
+    id: 1,
+    name: 'Action (set A)',
+    cards: [
+      {
+        id: 1,
+        word: 'cry',
+        translation: 'плакать',
+        image: '/img/cry.jpg',
+        audioSrc: '/audio/cry.mp3',
+      },
+      {
+        id: 2,
+        word: 'dance',
+        translation: 'танцевать',
+        image: '/img/dance.jpg',
+        audioSrc: '/audio/dance.mp3',
+      },
+      {
+        id: 3,
+        word: 'dive',
+        translation: 'нырять',
+        image: '/img/dive.jpg',
+        audioSrc: '/audio/dive.mp3',
+      },
+      {
+        id: 4,
+        word: 'draw',
+        translation: 'рисовать',
+        image: '/img/draw.jpg',
+        audioSrc: '/audio/draw.mp3',
+      },
+      {
+        id: 5,
+        word: 'fish',
+        translation: 'ловить рыбу',
+        image: '/img/fish.jpg',
+        audioSrc: '/audio/fish.mp3',
+      },
+      {
+        id: 6,
+        word: 'fly',
+        translation: 'летать',
+        image: '/img/fly.jpg',
+        audioSrc: '/audio/fly.mp3',
+      },
+      {
+        id: 7,
+        word: 'hug',
+        translation: 'обнимать',
+        image: '/img/hug.jpg',
+        audioSrc: '/audio/hug.mp3',
+      },
+      {
+        id: 8,
+        word: 'jump',
+        translation: 'прыгать',
+        image: '/img/jump.jpg',
+        audioSrc: '/audio/jump.mp3',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Action (set B)',
+    cards: [
+      {
+        id: 9,
+        word: 'open',
+        translation: 'открывать',
+        image: '/img/open.jpg',
+        audioSrc: '/audio/open.mp3',
+      },
+      {
+        id: 10,
+        word: 'play',
+        translation: 'играть',
+        image: '/img/play.jpg',
+        audioSrc: '/audio/play.mp3',
+      },
+      {
+        id: 11,
+        word: 'point',
+        translation: 'указывать',
+        image: '/img/point.jpg',
+        audioSrc: '/audio/point.mp3',
+      },
+      {
+        id: 12,
+        word: 'ride',
+        translation: 'ездить',
+        image: '/img/ride.jpg',
+        audioSrc: '/audio/ride.mp3',
+      },
+      {
+        id: 13,
+        word: 'run',
+        translation: 'бегать',
+        image: '/img/run.jpg',
+        audioSrc: '/audio/run.mp3',
+      },
+      {
+        id: 14,
+        word: 'sing',
+        translation: 'петь',
+        image: '/img/sing.jpg',
+        audioSrc: '/audio/sing.mp3',
+      },
+      {
+        id: 15,
+        word: 'skip',
+        translation: 'пропускать, прыгать',
+        image: '/img/skip.jpg',
+        audioSrc: '/audio/skip.mp3',
+      },
+      {
+        id: 16,
+        word: 'swim',
+        translation: 'плавать',
+        image: '/img/swim.jpg',
+        audioSrc: '/audio/swim.mp3',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Animal (set A)',
+    cards: [
+      {
+        id: 17,
+        word: 'cat',
+        translation: 'кот',
+        image: '/img/cat.jpg',
+        audioSrc: '/audio/cat.mp3',
+      },
+      {
+        id: 18,
+        word: 'chick',
+        translation: 'цыплёнок',
+        image: '/img/chick.jpg',
+        audioSrc: '/audio/chick.mp3',
+      },
+      {
+        id: 19,
+        word: 'chicken',
+        translation: 'курица',
+        image: '/img/chicken.jpg',
+        audioSrc: '/audio/chicken.mp3',
+      },
+      {
+        id: 20,
+        word: 'dog',
+        translation: 'собака',
+        image: '/img/dog.jpg',
+        audioSrc: '/audio/dog.mp3',
+      },
+      {
+        id: 21,
+        word: 'horse',
+        translation: 'лошадь',
+        image: '/img/horse.jpg',
+        audioSrc: '/audio/horse.mp3',
+      },
+      {
+        id: 22,
+        word: 'pig',
+        translation: 'свинья',
+        image: '/img/pig.jpg',
+        audioSrc: '/audio/pig.mp3',
+      },
+      {
+        id: 23,
+        word: 'rabbit',
+        translation: 'кролик',
+        image: '/img/rabbit.jpg',
+        audioSrc: '/audio/rabbit.mp3',
+      },
+      {
+        id: 24,
+        word: 'sheep',
+        translation: 'овца',
+        image: '/img/sheep.jpg',
+        audioSrc: '/audio/sheep.mp3',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Animal (set B)',
+    cards: [
+      {
+        id: 25,
+        word: 'bird',
+        translation: 'птица',
+        image: '/img/bird.jpg',
+        audioSrc: '/audio/bird.mp3',
+      },
+      {
+        id: 26,
+        word: 'fish',
+        translation: 'рыба',
+        image: '/img/fish1.jpg',
+        audioSrc: '/audio/fish.mp3',
+      },
+      {
+        id: 27,
+        word: 'frog',
+        translation: 'жаба',
+        image: '/img/frog.jpg',
+        audioSrc: '/audio/frog.mp3',
+      },
+      {
+        id: 28,
+        word: 'giraffe',
+        translation: 'жирафа',
+        image: '/img/giraffe.jpg',
+        audioSrc: '/audio/giraffe.mp3',
+      },
+      {
+        id: 29,
+        word: 'lion',
+        translation: 'лев',
+        image: '/img/lion.jpg',
+        audioSrc: '/audio/lion.mp3',
+      },
+      {
+        id: 30,
+        word: 'mouse',
+        translation: 'мышь',
+        image: '/img/mouse.jpg',
+        audioSrc: '/audio/mouse.mp3',
+      },
+      {
+        id: 31,
+        word: 'turtle',
+        translation: 'черепаха',
+        image: '/img/turtle.jpg',
+        audioSrc: '/audio/turtle.mp3',
+      },
+      {
+        id: 32,
+        word: 'dolphin',
+        translation: 'дельфин',
+        image: '/img/dolphin.jpg',
+        audioSrc: '/audio/dolphin.mp3',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Clothes',
+    cards: [
+      {
+        id: 33,
+        word: 'skirt',
+        translation: 'юбка',
+        image: '/img/skirt.jpg',
+        audioSrc: '/audio/skirt.mp3',
+      },
+      {
+        id: 34,
+        word: 'pants',
+        translation: 'брюки',
+        image: '/img/pants.jpg',
+        audioSrc: '/audio/pants.mp3',
+      },
+      {
+        id: 35,
+        word: 'blouse',
+        translation: 'блузка',
+        image: '/img/blouse.jpg',
+        audioSrc: '/audio/blouse.mp3',
+      },
+      {
+        id: 36,
+        word: 'dress',
+        translation: 'платье',
+        image: '/img/dress.jpg',
+        audioSrc: '/audio/dress.mp3',
+      },
+      {
+        id: 37,
+        word: 'boot',
+        translation: 'ботинок',
+        image: '/img/boot.jpg',
+        audioSrc: '/audio/boot.mp3',
+      },
+      {
+        id: 38,
+        word: 'shirt',
+        translation: 'рубашка',
+        image: '/img/shirt.jpg',
+        audioSrc: '/audio/shirt.mp3',
+      },
+      {
+        id: 39,
+        word: 'coat',
+        translation: 'пальто',
+        image: '/img/coat.jpg',
+        audioSrc: '/audio/coat.mp3',
+      },
+      {
+        id: 40,
+        word: 'shoe',
+        translation: 'туфли',
+        image: '/img/shoe.jpg',
+        audioSrc: '/audio/shoe.mp3',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Emotions',
+    cards: [
+      {
+        id: 41,
+        word: 'sad',
+        translation: 'грустный',
+        image: '/img/sad.jpg',
+        audioSrc: '/audio/sad.mp3',
+      },
+      {
+        id: 42,
+        word: 'angry',
+        translation: 'сердитый',
+        image: '/img/angry.jpg',
+        audioSrc: '/audio/angry.mp3',
+      },
+      {
+        id: 43,
+        word: 'happy',
+        translation: 'счастливый',
+        image: '/img/happy.jpg',
+        audioSrc: '/audio/happy.mp3',
+      },
+      {
+        id: 44,
+        word: 'tired',
+        translation: 'уставший',
+        image: '/img/tired.jpg',
+        audioSrc: '/audio/tired.mp3',
+      },
+      {
+        id: 45,
+        word: 'surprised',
+        translation: 'удивлённый',
+        image: '/img/surprised.jpg',
+        audioSrc: '/audio/surprised.mp3',
+      },
+      {
+        id: 46,
+        word: 'scared',
+        translation: 'испуганный',
+        image: '/img/scared.jpg',
+        audioSrc: '/audio/scared.mp3',
+      },
+      {
+        id: 47,
+        word: 'smile',
+        translation: 'улыбка',
+        image: '/img/smile.jpg',
+        audioSrc: '/audio/smile.mp3',
+      },
+      {
+        id: 48,
+        word: 'laugh',
+        translation: 'смех',
+        image: '/img/laugh.jpg',
+        audioSrc: '/audio/laugh.mp3',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'IT terms',
+    cards: [
+      {
+        id: 49,
+        word: 'developer',
+        translation: 'разработчик',
+        image: '/img/developer.jpg',
+        audioSrc: '/audio/developer.mp3',
+      },
+      {
+        id: 50,
+        word: 'deadline',
+        translation: 'срок',
+        image: '/img/deadline.jpg',
+        audioSrc: '/audio/deadline.mp3',
+      },
+      {
+        id: 51,
+        word: 'fix',
+        translation: 'исправить',
+        image: '/img/fix.jpg',
+        audioSrc: '/audio/fix.mp3',
+      },
+      {
+        id: 52,
+        word: 'user experience',
+        translation: 'пользовательский опыт',
+        image: '/img/user-experience.jpg',
+        audioSrc: '/audio/user-experience.mp3',
+      },
+      {
+        id: 53,
+        word: 'digital',
+        translation: 'цифровой',
+        image: '/img/digital.jpg',
+        audioSrc: '/audio/digital.mp3',
+      },
+      {
+        id: 54,
+        word: 'cookies',
+        translation: 'печенье',
+        image: '/img/cookies.jpg',
+        audioSrc: '/audio/cookies.mp3',
+      },
+      {
+        id: 55,
+        word: 'comments',
+        translation: 'комментарии',
+        image: '/img/comments.jpg',
+        audioSrc: '/audio/comments.mp3',
+      },
+      {
+        id: 56,
+        word: 'implimentation',
+        translation: 'реализация',
+        image: '/img/implimentation.jpg',
+        audioSrc: '/audio/implimentation.mp3',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Beach',
+    cards: [
+      {
+        id: 57,
+        word: 'shark',
+        translation: 'акула',
+        image: '/img/shark.jpg',
+        audioSrc: '/audio/shark.mp3',
+      },
+      {
+        id: 58,
+        word: 'catamaran',
+        translation: 'катамаран',
+        image: '/img/catamaran.jpg',
+        audioSrc: '/audio/catamaran.mp3',
+      },
+      {
+        id: 59,
+        word: 'crab',
+        translation: 'краб',
+        image: '/img/crab.jpg',
+        audioSrc: '/audio/crab.mp3',
+      },
+      {
+        id: 60,
+        word: 'palm',
+        translation: 'пальма',
+        image: '/img/palm.jpg',
+        audioSrc: '/audio/palm.mp3',
+      },
+      {
+        id: 61,
+        word: 'wave',
+        translation: 'волна',
+        image: '/img/wave.jpg',
+        audioSrc: '/audio/wave.mp3',
+      },
+      {
+        id: 62,
+        word: 'gull',
+        translation: 'чайка',
+        image: '/img/gull.jpg',
+        audioSrc: '/audio/gull.mp3',
+      },
+      {
+        id: 63,
+        word: 'lifeguard',
+        translation: 'спасатель',
+        image: '/img/lifeguard.jpg',
+        audioSrc: '/audio/lifeguard.mp3',
+      },
+      {
+        id: 64,
+        word: 'sand',
+        translation: 'песок',
+        image: '/img/sand.jpg',
+        audioSrc: '/audio/sand.mp3',
+      },
+    ],
+  },
+];
+
+export function getCategories(): Promise<Category[]> {
+  return Promise.resolve<Category[]>(categories);
+}
+
+export function getCategoryById(
+  categoryId: number,
+): Promise<Category | undefined> {
+  return Promise.resolve(
+    categories.find(category => category.id === categoryId),
+  );
+}
+
+export function createCategory(newCategory: Category): Promise<Category> {
+  const isExist =
+    typeof categories.find(
+      category =>
+        newCategory.name.toLowerCase() === category.name.toLowerCase(),
+    ) !== 'undefined';
+  if (isExist) {
+    return Promise.reject(
+      new Error(`Category with name ${newCategory.name} is already exists`),
+    );
+  }
+
+  const id = categories.length + 1;
+  const model = { ...newCategory, id };
+  categories.push(model);
+
+  return Promise.resolve(model);
+}
+
+export function deleteCategory(id: number): Promise<void> {
+  const index = categories.findIndex(category => category.id === id);
+  if (index < 0) {
+    Promise.reject(new Error('Category not found'));
+  }
+  categories.splice(index, 1);
+  return Promise.resolve();
+}
+
+export function updateCategory(freshCategory: Category): Promise<Category> {
+  const index = categories.findIndex(
+    category => category.id === freshCategory.id,
+  );
+  if (index < 0) {
+    Promise.reject(new Error('Category not found'));
+  }
+  categories[index] = freshCategory;
+  return Promise.resolve(categories[index]);
+}
